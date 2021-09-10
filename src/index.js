3f4a15c70b5eef3b75b328f3feab8e8e05d73cc5
DMawi17 <asegda17@gmail.com>
2021-09-10 14:21:47 +0200

    index modified

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 import "./style.css";
 import "regenerator-runtime";
 
-const latinLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'
-					, 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'
-					, 'S', 'T', 'U', 'W', 'X', 'Y', 'Z'];
-
-
-
